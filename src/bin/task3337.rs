88d666d977d710f41cd9b1d42e4fa92bffb5db1d
Eugine Kosenko <eugine.kosenko@gmail.com>
2025-05-14 19:01:54 +0300

    Task 3337 resolved

---
+++ src/bin/task3337.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::ops::{Deref, DerefMut};
+
+const MOD: u64 = 10u64.pow(9) + 7;
+#[derive(Clone, Debug)]
+struct Matrix(Vec<Vec<u64>>);
+
+impl Matrix {
+    fn new(n: usize) -> Self { Self(vec![vec![0; 26]; n]) }
+    fn row(row: Vec<u64>) -> Self { Self(vec![row]) }
+    fn pow(&self, n: i32) -> Self {
+        if n == 1 { return self.clone(); }
+        let y = self.pow(n/2);
+        let y = mul(&y, &y);
+        if n % 2 == 0 { y } else { mul(&y, self) }
+    }
+}
+impl Deref for Matrix {
+    type Target = Vec<Vec<u64>>;
+    fn deref(&self) -> &Self::Target { &self.0 }
+}
+
+impl DerefMut for Matrix {
+    fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
+}
+fn mul(lhs: &Matrix, rhs: &Matrix) -> Matrix {
+    let m = lhs.len();
+    let mut result = Matrix::new(m);
+    for i in 0..m { for j in 0..26 { for k in 0..26 {
+        result[i][j] = (result[i][j] + (lhs[i][k] * rhs[k][j]) % MOD) % MOD;
+    } } }
+    result
+}
+
+fn length_after_transformations(s: String, t: i32, nums: Vec<i32>) -> i32 {
+    let cs = Matrix::row(
+        s.chars().fold(vec![0; 26], |mut cs, c| { cs[(c as u8 - b'a') as usize] += 1; cs }));
+    let tr = (0..26).fold(Matrix::new(26), |mut tr, i| {
+        for j in i+1..=i+(nums[i] as usize) { tr[i][j%26] = 1; }
+        tr });
+    mul(&cs, &tr.pow(t))[0].iter().fold(0, |a, b| (a+*b) % MOD) as i32
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let s = args[1].to_string();
+    let t = args[2].parse().unwrap();
+    let nums = args[3]
+        .trim_start_matches('[')
+        .trim_end_matches(']')
+        .split(',')
+        .map(|item| item.parse().unwrap())
+        .collect();
+    println!("{}", length_after_transformations(s, t, nums));
+}
